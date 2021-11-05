c6d096774fc553a9f1b5bc3f541018066a63dc0c
MrDoctor-666 <sheron.dragnil@mai.ru>
2021-11-05 21:02:29 +0300

    lab2. Change variables' names

--- src/main/kotlin/lab2/Calculator.kt
+++ src/main/kotlin/lab2/Calculator.kt
@@ -7,9 +7,9 @@ import kotlin.math.PI
 
 class Calculator(private val expression: String) {
 
-    private val _operators = hashSetOf("+", "-", "*", "/", "^")
-    private val _constants = mapOf("pi" to PI, "e" to E)
-    private val _rpn: MutableList<String> = mutableListOf()
+    private val operators = hashSetOf("+", "-", "*", "/", "^")
+    private val constants = mapOf("pi" to PI, "e" to E)
+    private val rpn: MutableList<String> = mutableListOf()
 
     fun calc(): Double {
         checkBrackets()
@@ -34,30 +34,30 @@ class Calculator(private val expression: String) {
                 .replace("-(", "0-(")
         val stack: Stack<String> = Stack()
         var index = 0
-        _rpn.clear()
+        rpn.clear()
 
         while (index < stringToWork.length) {
             when (stringToWork[index]) {
                 '(' -> stack.push(stringToWork[index].toString())
                 ')' -> {
                     //adding from stack to result until we find ( in stack
-                    while (stack.peek() != "(") _rpn.add(stack.pop())
+                    while (stack.peek() != "(") rpn.add(stack.pop())
                     stack.pop() //delete ( from stack
                 }
                 '+', '-' -> {
                     //unary minus and plus
                     // if it's first in string, or it's after another operator or (
                     //there should be a number after
-                    if (index == 0 || _operators.contains(stringToWork[index - 1].toString())
+                    if (index == 0 || operators.contains(stringToWork[index - 1].toString())
                         || stringToWork[index - 1] == '('
                     ) {
                         val indexEnd = findNumberEnd(stringToWork, index + 1)
-                        _rpn.add(stringToWork.substring(index, indexEnd + 1))
+                        rpn.add(stringToWork.substring(index, indexEnd + 1))
                         index = indexEnd
                     }
                     //binary
                     else {
-                        while (stack.isNotEmpty() && _operators.contains(stack.peek())) _rpn.add(stack.pop())
+                        while (stack.isNotEmpty() && operators.contains(stack.peek())) rpn.add(stack.pop())
                         stack.push(stringToWork[index].toString())
                     }
                 }
@@ -65,7 +65,7 @@ class Calculator(private val expression: String) {
                     while (stack.isNotEmpty() &&
                         (stack.peek() == "*" || stack.peek() == "/" || stack.peek() == "^")
                     )
-                        _rpn.add(stack.pop())
+                        rpn.add(stack.pop())
                     stack.push(stringToWork[index].toString())
                 }
                 '^' -> stack.push(stringToWork[index].toString())
@@ -75,8 +75,8 @@ class Calculator(private val expression: String) {
                     while (indexEnd + 1 < stringToWork.length && stringToWork[indexEnd + 1].isLetter())
                         indexEnd++
                     val str = stringToWork.substring(index, indexEnd + 1).lowercase()
-                    //checking letter sequence that equals constant in _constants
-                    if (_constants.containsKey(str)) _rpn.add(_constants[str].toString())
+                    //checking letter sequence that equals constant in constants
+                    if (constants.containsKey(str)) rpn.add(constants[str].toString())
                     //if we didn't find a constant in letters, then something is wrong
                     else throw Error("Unrecognizable letters")
 
@@ -85,7 +85,7 @@ class Calculator(private val expression: String) {
                 in '0'..'9' -> {
                     //adding number to result
                     val indexEnd = findNumberEnd(stringToWork, index)
-                    _rpn.add(stringToWork.substring(index, indexEnd + 1))
+                    rpn.add(stringToWork.substring(index, indexEnd + 1))
                     index = indexEnd
                 }
                 else -> throw Error("Wrong symbols in input")
@@ -93,24 +93,24 @@ class Calculator(private val expression: String) {
             index++
         }
 
-        while (stack.isNotEmpty()) _rpn.add(stack.pop())
+        while (stack.isNotEmpty()) rpn.add(stack.pop())
     }
 
     private fun countFromRPN(): Double {
         var result: Double
         val stack: Stack<Double> = Stack()
 
-        while (_rpn.isNotEmpty()) {
-            if (_rpn.first().toDoubleOrNull() == null) {
+        while (rpn.isNotEmpty()) {
+            if (rpn.first().toDoubleOrNull() == null) {
                 //operator
                 if (stack.size > 1) {
-                    result = doOperation(stack.pop(), stack.pop(), _rpn.first())
+                    result = doOperation(stack.pop(), stack.pop(), rpn.first())
                     stack.push(result)
                 } else throw Error("Not enough arguments")
             } else {
-                stack.push(_rpn.first().toDouble())
+                stack.push(rpn.first().toDouble())
             }
-            _rpn.removeFirst()
+            rpn.removeFirst()
         }
 
         return stack.pop()
